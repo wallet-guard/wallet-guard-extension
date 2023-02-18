773e27d46105f84b8d8689696cdc80710fa937ba
mindofmar <martinpeko777@gmail.com>
2023-02-18 14:15:59 -0500

    filter out browser native pages

--- src/services/phishing/phishingService.ts
+++ src/services/phishing/phishingService.ts
@@ -14,9 +14,11 @@ export async function checkUrlForPhishing(tab: chrome.tabs.Tab) {
 
   const domainName = getDomainNameFromURL(url);
   const personalWhitelist = await localStorageHelpers.get<string[]>(WgKeys.PersonalWhitelist);
+  // do not scan if domain is on personal whitelist
   if (personalWhitelist?.includes(domainName)) return;
 
-  if (url === '' || url.startsWith('chrome://') || url.startsWith('chrome-extension://')) return;
+  // do not scan if domain is browser native page
+  if (hasBrowserPrefix(url)) return;
 
   const pdsResponse = await domainScan(url);
   chrome.storage.local.set({ currentSite: pdsResponse });
@@ -75,4 +77,19 @@ export async function checkUrlForPhishing(tab: chrome.tabs.Tab) {
 
     AlertHandler.create(activityInfo);
   }
+}
+
+
+function hasBrowserPrefix(input: string): boolean {
+  const isChrome = input.startsWith('chrome://') || input.startsWith('chrome-extension://');
+  const isBrave = input.startsWith('brave://');
+  const isEdge = input.startsWith('edge://') || input.startsWith('about:blank');
+  const isLocalhost = input.startsWith('localhost:') || input.startsWith('http://localhost:');
+  const isFile = input.startsWith('file://');
+
+  if (isChrome || isBrave || isEdge || isLocalhost || isFile) {
+    return true;
+  }
+
+  return false;
 }
