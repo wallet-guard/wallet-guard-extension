e67e3d23f5b6bb41a045c2f54945bb7f7dd542ae
mindofmar <martinpeko777@gmail.com>
2023-03-12 20:28:39 -0400

    add component for reverts

--- src/components/simulation/Error.tsx
+++ src/components/simulation/Error.tsx
@@ -7,6 +7,7 @@ import { SimulationHeader } from './SimulationHeader';
 import { SimulationOverview } from './SimulationOverview';
 import GeneralErrorComponent from './SimulationSubComponents/errors/GeneralError';
 import InsufficientFundsComponent from './SimulationSubComponents/errors/InsufficientFundsError';
+import RevertComponent from './SimulationSubComponents/errors/RevertError';
 
 interface ErrorComponentProps {
   filteredSimulations: StoredSimulation[];
@@ -19,6 +20,19 @@ export const ErrorComponent = (props: ErrorComponentProps) => {
   posthog.capture('show simulation error', {
     filteredSimulations: filteredSimulations,
   });
+
+  function getErrorComponent() {
+    switch (type) {
+      case ErrorType.InsufficientFunds:
+        return <InsufficientFundsComponent filteredSimulations={filteredSimulations} />;
+
+      case ErrorType.Revert:
+        return <RevertComponent filteredSimulations={filteredSimulations} />;
+
+      default:
+        return <GeneralErrorComponent filteredSimulations={filteredSimulations} />;
+    }
+  }
 
   return (
     <div>
@@ -44,11 +58,8 @@ export const ErrorComponent = (props: ErrorComponentProps) => {
           <img src="/images/popup/simulation_error.png" alt="error" width={150} />
         </div>
       </div>
-      {type === ErrorType.InsufficientFunds ? (
-        <InsufficientFundsComponent filteredSimulations={filteredSimulations} />
-      ) : (
-        <GeneralErrorComponent filteredSimulations={filteredSimulations} />
-      )}
+
+      {getErrorComponent()}
 
       <ConfirmSimulationButton storedSimulation={filteredSimulations && filteredSimulations[0]} />
     </div>
