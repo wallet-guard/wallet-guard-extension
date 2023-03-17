e1ebf8c181535910332d7ec957d4f5b01cfc2e09
ultimate <106779544+0xultimate@users.noreply.github.com>
2023-03-17 00:13:00 -0500

    refactor: break into components

--- src/components/app-dashboard/tabs/chatweb3/ChatWeb3Details.tsx
+++ src/components/app-dashboard/tabs/chatweb3/ChatWeb3Details.tsx
@@ -97,71 +97,62 @@ export const logo = (
   </svg>
 );
 
-export const ChatWeb3Details = () => {
+interface ChatWeb3DetailTextBoxProps {
+  children: string;
+}
+
+interface ChatWeb3DetailsColumnProps {
+  title: string;
+  image: JSX.Element;
+  children: React.ReactNode;
+}
+
+const ChatWeb3DetailsColumn = (props: ChatWeb3DetailsColumnProps) => {
   return (
     <div
       style={{
-        display: 'flex',
-        flexDirection: 'column',
+        maxWidth: '225px',
         alignItems: 'center',
-        width: '100%',
-        paddingTop: '60px',
+        justifyContent: 'center',
+        textAlign: 'center',
       }}
     >
-      <div style={{ display: 'flex', flexDirection: 'row', gap: '58px' }}>
-        <div
-          className="row"
-          style={{
-            width: '240px',
-            height: '170px',
-            alignItems: 'center',
-            justifyContent: 'center',
-            textAlign: 'center',
-          }}
-        >
-          <div style={{ position: 'absolute', marginTop: '-195px' }}>{sun}</div>
+      <div className={styles.flexCenter}>{sun}</div>
+      <p style={{ fontSize: '1.2rem', marginTop: '5px' }}>{props.title}</p>
+      {props.children}
+    </div>
+  );
+};
 
-          <p style={{ fontSize: '1.2rem', marginTop: '5px' }}>Use Cases</p>
 
-          <div className={`${styles.details} hiderow `}>Get answers to any web3 related question</div>
-          <div className={`${styles.details} hiderow`}>Here to help protect you from scams</div>
-          <div className={`${styles.details} hiderow`}>Translate difficult web3 concepts into plain English</div>
-        </div>
-        <div
-          className="row hiderow"
-          style={{
-            width: '225px',
-            height: '150px',
-            alignItems: 'center',
-            justifyContent: 'center',
-            textAlign: 'center',
-          }}
-        >
-          <div style={{ position: 'absolute', marginTop: '-175px' }}>{thunder}</div>
 
-          <p style={{ fontSize: '1.2rem', marginTop: '5px' }}>Capabilities</p>
-          <div className={`${styles.details} hiderow`}>Knowledge & Guidance</div>
-          <div className={`${styles.details} hiderow`}>Security first approach</div>
-          <div className={`${styles.details} hiderow`}>Blockchain & transaction analytics</div>
-        </div>
-        <div
-          className="row hiderow"
-          style={{
-            width: '225px',
-            height: '150px',
-            alignItems: 'center',
-            justifyContent: 'center',
-            textAlign: 'center',
-          }}
-        >
-          <div style={{ position: 'absolute', marginTop: '-175px' }}>{alert}</div>
-
-          <p style={{ fontSize: '1.2rem', marginTop: '5px' }}>Limitations</p>
-          <div className={`${styles.details} hiderow`}>May occasionally generate incorrect information</div>
-          <div className={`${styles.details} hiderow`}>May produce biased content</div>
-          <div className={`${styles.details} hiderow`}>Always do your own research</div>
-        </div>
-      </div>
+export const ChatWeb3Details = () => {
+const ChatWeb3DetailTextBox = (props: ChatWeb3DetailTextBoxProps) => {
+  return <div className={`${styles.details} ${styles.flexCenter}`}>{props.children}</div>;
+};
+  return (
+    <div
+      className={styles.flexCenter}
+      style={{
+        marginTop: '60px',
+        gap: '30px',
+      }}
+    >
+      <ChatWeb3DetailsColumn title="Use Cases" image={sun}>
+        <ChatWeb3DetailTextBox>Get answers to any web3 related question</ChatWeb3DetailTextBox>
+        <ChatWeb3DetailTextBox>Here to help protect you from scams</ChatWeb3DetailTextBox>
+        <ChatWeb3DetailTextBox>Translate difficult web3 concepts into plain English</ChatWeb3DetailTextBox>
+      </ChatWeb3DetailsColumn>
+      <ChatWeb3DetailsColumn title="Capabilities" image={thunder}>
+        <ChatWeb3DetailTextBox>Knowledge & Guidance</ChatWeb3DetailTextBox>
+        <ChatWeb3DetailTextBox>Security first approach</ChatWeb3DetailTextBox>
+        <ChatWeb3DetailTextBox>Blockchain & transaction analytics</ChatWeb3DetailTextBox>
+      </ChatWeb3DetailsColumn>
+      <ChatWeb3DetailsColumn title="Limitations" image={alert}>
+        <ChatWeb3DetailTextBox>May occasionally generate incorrect information</ChatWeb3DetailTextBox>
+        <ChatWeb3DetailTextBox>May produce biased content</ChatWeb3DetailTextBox>
+        <ChatWeb3DetailTextBox>Always do your own research</ChatWeb3DetailTextBox>
+      </ChatWeb3DetailsColumn>
     </div>
   );
 };
