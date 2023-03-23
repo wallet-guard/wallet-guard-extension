121d0ec2d4f503195dcd9501433d1bfb7ed1fb16
Jacob Lebowitz <jacob@walletguard.app>
2023-03-23 15:14:18 -0500

    cleanup

--- src/components/app-dashboard/tabs/chatweb3/temp/pages/index.tsx
+++ src/components/app-dashboard/tabs/chatweb3/temp/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   const [lightMode, setLightMode] = useState<'dark' | 'light'>('dark');
   const [messageIsStreaming, setMessageIsStreaming] = useState<boolean>(false);
   const [showSidebar, setShowSidebar] = useState<boolean>(true);
-  const [apiKey, setApiKey] = useState<string>('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');
+  const [apiKey, setApiKey] = useState<string>('');
   const [messageError, setMessageError] = useState<boolean>(false);
   const [modelError, setModelError] = useState<boolean>(false);
   const stopConversationRef = useRef<boolean>(false);
