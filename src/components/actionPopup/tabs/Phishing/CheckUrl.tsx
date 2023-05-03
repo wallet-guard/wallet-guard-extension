84391cfd20df6b19a77c5ad6b004fae27bdf35b4
mindofmar <martinpeko777@gmail.com>
2023-05-03 18:18:04 -0500

    finish phishing footer with css

--- src/components/actionPopup/tabs/Phishing/CheckUrl.tsx
+++ src/components/actionPopup/tabs/Phishing/CheckUrl.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styles from '../../ActionPopup.module.css';
-import { Button, ChakraProvider, Input, InputGroup, InputRightAddon, InputRightElement } from '@chakra-ui/react';
+import { Button, Input } from '@chakra-ui/react';
 
 export const URLCheckerInput = () => {
   const [inputValue, setInputValue] = useState('');
@@ -9,12 +9,23 @@ export const URLCheckerInput = () => {
   return (
     <div className={styles.footer}>
       <div className="container">
-        <div className="justify-content-center align-items-center">
-          <p className="text-muted">Input any URL to lookup</p>
-          <Input value={inputValue} onChange={handleChange} placeholder="coinbase.com" size="sm" />
-          <Button size="md" color="green.200" variant="solid">
-            Check
-          </Button>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+          <p className="text-muted" style={{ fontSize: '16px' }}>
+            Input any URL to lookup
+          </p>
+          <div className="row" style={{ width: '372px' }}>
+            <input
+              className={styles.urlCheckInput}
+              placeholder="https://coinbase.com"
+              onChange={handleChange}
+              value={inputValue}
+            />
+            <button className={styles.urlCheckButton}>CHECK</button>
+            {/* <Input value={inputValue} onChange={handleChange} placeholder="coinbase.com" size="sm" />
+            <Button disabled={inputValue === ''} size="md" color="green.200" variant="solid">
+              Check
+            </Button> */}
+          </div>
         </div>
       </div>
     </div>
--- src/components/actionPopup/tabs/Phishing/PhishingTabContainer.tsx
+++ src/components/actionPopup/tabs/Phishing/PhishingTabContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { PhishingResponse, PhishingResult, Warning } from '../../../../models/PhishingResponse';
 
 interface PhishingTabContainerProps {
