43f102b63004cba1b7d90bfbb1a4d79be347fa55
mindofmar <martinpeko777@gmail.com>
2023-05-17 16:04:14 -0500

    integrate alert count to header

--- src/components/actionPopup/common/PopupHeader.tsx
+++ src/components/actionPopup/common/PopupHeader.tsx
@@ -1,12 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styles from '../ActionPopup.module.css';
 import { openDashboard } from '../../../lib/helpers/linkHelper';
 import { FiBell, FiSettings } from 'react-icons/fi';
 import { PopupTabContext } from '../../../lib/context/context';
 import { ActionPopupTab } from '../../../models/actionPopupScreen';
+import { AlertHandler } from '../../../lib/helpers/chrome/alertHandler';
+import { AlertDetail } from '../../../models/Alert';
 
 export const PopupHeader = () => {
   const { currentTab, updateTab } = useContext(PopupTabContext);
+  const [unreadAlerts, setUnreadAlerts] = useState<AlertDetail[]>([]);
+
+  useEffect(() => {
+    AlertHandler.getAllUnreadAlerts().then((_alertsFeed) => setUnreadAlerts(_alertsFeed));
+  }, []);
 
   function toggleSettingsTab() {
     if (currentTab === ActionPopupTab.SettingsTab) {
@@ -22,7 +29,10 @@ export const PopupHeader = () => {
       <img src="/images/wg_logos/Wallpaper-Transparent.png" alt="" width={'140px'} />
 
       <div className={styles.iconsRow}>
-        <FiBell className={styles.headerIcon} onClick={() => openDashboard('popup', false)} />
+        <div className={styles.alertsButton}>
+          {unreadAlerts.length > 0 && <span className={styles.badge}>{unreadAlerts.length}</span>}
+          <FiBell className={styles.headerIcon} onClick={() => openDashboard('popup', false)} />
+        </div>
         <FiSettings className={styles.headerIcon} onClick={toggleSettingsTab} />
       </div>
     </div>
