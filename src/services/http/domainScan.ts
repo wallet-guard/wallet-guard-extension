0879705c637118620e00cdc5ae87ab228c2c50ac
mindofmar <martinpeko777@gmail.com>
2023-05-27 23:28:29 -0500

    update old ref

--- src/services/http/domainScan.ts
+++ src/services/http/domainScan.ts
@@ -1,10 +1,10 @@
-import { PDS_SERVER_URL_PROD } from '../../lib/environment';
+import { SERVER_URL_PROD } from '../../lib/environment';
 import { PhishingResponse } from '../../models/PhishingResponse';
 import * as httpClient from '../clients/httpClient';
 
 export async function domainScan(url: string): Promise<PhishingResponse | null> {
   try {
-    const res = await httpClient.get(`${PDS_SERVER_URL_PROD}/scan?url=${url}`);
+    const res = await httpClient.get(`${SERVER_URL_PROD}/scan?url=${url}`);
     const json: PhishingResponse = (await res?.json()) || null;
     return json;
   } catch (e) {
