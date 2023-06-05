db988d268cdccb08a378b2f96bae7cab39c74901
mindofmar <martinpeko777@gmail.com>
2023-06-05 13:50:35 -0400

    add opensea test case

--- src/tests/convertObject.test.ts
+++ src/tests/convertObject.test.ts
@@ -79,7 +79,7 @@ describe('convertObjectValuesToString', () => {
                 "origin": "",
                 "primaryType": "PermitBatch",
                 "signer": "0x12345"
-            }`
+            }`;
 
 
         const testObj = JSON.parse(badRequest);
@@ -89,58 +89,130 @@ describe('convertObjectValuesToString', () => {
         expect(expectedObj).toEqual(output);
     });
 
-    it('handles Permit2', () => {
+    it('handles Seaport OpenComponents', () => {
         const badRequest = `{
-                "chainId": "0x1",
+                "chainId": "0x89",
                 "domain": {
-                    "chainId": "1",
-                    "name": "Permit2",
-                    "verifyingContract": "0xasdf"
+                    "chainId": "137",
+                    "name": "Seaport",
+                    "verifyingContract": "0x00000000000000adc04c56bf30ac9d3c0aaf14dc",
+                    "version": "1.5"
                 },
                 "id": "",
                 "message": {
-                    "details": [
+                    "conduitKey": "0x0000001",
+                    "consideration": [
                         {
-                            "amount": "1461501637330902918203684832716283019655932542975",
-                            "expiration": 1684323541,
-                            "nonce": 0,
-                            "token": "0xdac17f958d2ee523a2206206994597c13d831ec7"
+                            "endAmount": "875000000000000000",
+                            "identifierOrCriteria": "0",
+                            "itemType": 0,
+                            "recipient": "0x12345",
+                            "startAmount": "875000000000000000",
+                            "token": "0x0000000000000000000000000000000000000000"
+                        },
+                        {
+                            "endAmount": "100000000000000000",
+                            "identifierOrCriteria": "0",
+                            "itemType": 0,
+                            "recipient": "0x67890",
+                            "startAmount": "100000000000000000",
+                            "token": "0x0000000000000000000000000000000000000000"
+                        },
+                        {
+                            "endAmount": "25000000000000000",
+                            "identifierOrCriteria": "0",
+                            "itemType": 0,
+                            "recipient": "0x00001",
+                            "startAmount": "25000000000000000",
+                            "token": "0x0000000000000000000000000000000000000000"
                         }
                     ],
-                    "sigDeadline": "1684323541",
-                    "spender": "0x98765"
+                    "counter": "0",
+                    "endTime": "1686403247",
+                    "offer": [
+                        {
+                            "endAmount": 1,
+                            "identifierOrCriteria": "1408",
+                            "itemType": 2,
+                            "startAmount": 1,
+                            "token": "0x980341uf093j410"
+                        }
+                    ],
+                    "offerer": "0x12345",
+                    "orderType": "0",
+                    "salt": "5158915845115",
+                    "startTime": "1685798447",
+                    "totalOriginalConsiderationItems": "3",
+                    "zone": "0x0000000000000000000000000000000000000000",
+                    "zoneHash": "0x0000000000000000000000000000000000000000000000000000000000000000"
                 },
                 "method": "eth_signTypedData_v4",
                 "origin": "",
-                "primaryType": "PermitBatch",
+                "primaryType": "OrderComponents",
                 "signer": "0x12345"
             }`;
 
         const expected = `{
-                "chainId": "0x1",
+                "chainId": "0x89",
                 "domain": {
-                    "chainId": "1",
-                    "name": "Permit2",
-                    "verifyingContract": "0xasdf"
+                    "chainId": "137",
+                    "name": "Seaport",
+                    "verifyingContract": "0x00000000000000adc04c56bf30ac9d3c0aaf14dc",
+                    "version": "1.5"
                 },
                 "id": "",
                 "message": {
-                    "details": [
+                    "conduitKey": "0x0000001",
+                    "consideration": [
                         {
-                            "amount": "1461501637330902918203684832716283019655932542975",
-                            "expiration": "1684323541",
-                            "nonce": "0",
-                            "token": "0xdac17f958d2ee523a2206206994597c13d831ec7"
+                            "endAmount": "875000000000000000",
+                            "identifierOrCriteria": "0",
+                            "itemType": "0",
+                            "recipient": "0x12345",
+                            "startAmount": "875000000000000000",
+                            "token": "0x0000000000000000000000000000000000000000"
+                        },
+                        {
+                            "endAmount": "100000000000000000",
+                            "identifierOrCriteria": "0",
+                            "itemType": "0",
+                            "recipient": "0x67890",
+                            "startAmount": "100000000000000000",
+                            "token": "0x0000000000000000000000000000000000000000"
+                        },
+                        {
+                            "endAmount": "25000000000000000",
+                            "identifierOrCriteria": "0",
+                            "itemType": "0",
+                            "recipient": "0x00001",
+                            "startAmount": "25000000000000000",
+                            "token": "0x0000000000000000000000000000000000000000"
                         }
                     ],
-                    "sigDeadline": "1684323541",
-                    "spender": "0x98765"
+                    "counter": "0",
+                    "endTime": "1686403247",
+                    "offer": [
+                        {
+                            "endAmount": "1",
+                            "identifierOrCriteria": "1408",
+                            "itemType": "2",
+                            "startAmount": "1",
+                            "token": "0x980341uf093j410"
+                        }
+                    ],
+                    "offerer": "0x12345",
+                    "orderType": "0",
+                    "salt": "5158915845115",
+                    "startTime": "1685798447",
+                    "totalOriginalConsiderationItems": "3",
+                    "zone": "0x0000000000000000000000000000000000000000",
+                    "zoneHash": "0x0000000000000000000000000000000000000000000000000000000000000000"
                 },
                 "method": "eth_signTypedData_v4",
                 "origin": "",
-                "primaryType": "PermitBatch",
+                "primaryType": "OrderComponents",
                 "signer": "0x12345"
-            }`
+            }`;
 
 
         const testObj = JSON.parse(badRequest);
