610646ab721155190cba7482266528f7a9eb4841
mar <72634565+mindofmar@users.noreply.github.com>
2023-06-07 00:02:35 -0500

    Better request type safety (#58)
    * add type safety
    
    * use href instead of hostname for urls
    
    * remove export
    
    * add null check
    
    * add opensea test case
    
    * add normal transaction example
    
    * fix minor css inconsistency

--- src/injected/injectWalletGuard.tsx
+++ src/injected/injectWalletGuard.tsx
@@ -13,7 +13,19 @@ export function convertObjectValuesToString(inputObj: any): any {
   const keys = Object.keys(inputObj);
   const output: any = {};
   for (let x of keys) {
-    if (typeof inputObj[x] === 'number' || typeof inputObj[x] === 'bigint') {
+    if (Array.isArray(inputObj[x])) {
+      const array: any[] = [];
+
+      for (let y in inputObj[x]) {
+        array.push(convertObjectValuesToString(inputObj[x][y]));
+      }
+
+      output[x] = array;
+    } else if (inputObj[x] === null) {
+      output[x] = null;
+    } else if (typeof inputObj[x] === 'object') {
+      output[x] = convertObjectValuesToString(inputObj[x]);
+    } else if (typeof inputObj[x] === 'number' || typeof inputObj[x] === 'bigint') {
       output[x] = String(inputObj[x]);
     } else {
       output[x] = inputObj[x];
