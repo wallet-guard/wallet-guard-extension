25fc8628dc876167c5c97642790847344c81b003
Jacob Lebowitz <jacob@walletguard.app>
2023-06-18 18:46:09 -0500

    small cleanup

--- src/components/app-dashboard/tabs/chatweb3/components/Chat/ChatWeb3Navbar.tsx
+++ src/components/app-dashboard/tabs/chatweb3/components/Chat/ChatWeb3Navbar.tsx
@@ -70,18 +70,3 @@ export const Navbar = ({
     </div>
   );
 };
-
-// return (
-//   <div
-//     className="bg-[#232323] h-16 flex items-center justify-between px-4"
-//     style={{ boxShadow: '0 2px 4px rgba(0, 0, 0, 0.25)' }}
-//   >
-//     <div className="flex items-center flex-1">
-//       <img className="h-8 w-auto" src="/images/wg_logos/Logo-Large-Transparent.png" alt="Logo" />
-//       <h1 className="text-white font-large text-lg ml-2">
-//         <b>ChatWeb3</b>
-//       </h1>
-//     </div>
-//   </div>
-// );
-// };
