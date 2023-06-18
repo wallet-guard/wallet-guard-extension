c2d6da36f93c5a18cb08ebe1db72f5fa8839d4fa
Jacob Lebowitz <jacob@walletguard.app>
2023-06-17 22:56:23 -0500

    small cleanup

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/chatweb3.html'],
-  darkMode: "class",
-  theme: {
-    extend: {}
-  },
-  plugins: [require("@tailwindcss/typography")]
-};
