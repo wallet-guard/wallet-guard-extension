2c8c86ae57568ed0954564f2d5abe5ec1a5d4f3a
mindofmar <martinpeko777@gmail.com>
2023-07-21 17:40:22 -0500

    use semibold across all error uis

--- src/components/simulation/SimulationSubComponents/errors/RevertError.tsx
+++ src/components/simulation/SimulationSubComponents/errors/RevertError.tsx
@@ -7,7 +7,7 @@ export default function RevertComponent(props: ErrorComponentProps) {
   return (
     <div className="row pt-5 text-center pl-4 pr-4">
       <div className="col-12">
-        <h4 className={`${styles['font-archivo-medium']} pb-3`} style={{ color: 'white', marginBottom: '20px' }}>
+        <h4 className={`${styles['font-archivo-semibold']} pb-3`} style={{ color: 'white', marginBottom: '20px' }}>
           The transaction will be reverted
         </h4>
       </div>
