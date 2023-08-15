cfbb1a2d7ff7e6d4470bbc365fc2e16f2ffefb30
mindofmar <martinpeko777@gmail.com>
2023-08-15 02:16:35 -0500

    use new gradient styling and implement report transaction button

--- src/components/simulation/TransactionDetails.tsx
+++ src/components/simulation/TransactionDetails.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './simulation.module.css';
 import { TransactionDetailLabel } from './SimulationSubComponents/transactionDetails/TransactionDetailLabel';
 import { ChainDetail } from './SimulationSubComponents/transactionDetails/ChainDetail';
@@ -7,9 +7,36 @@ import { WebsiteDetail } from './SimulationSubComponents/transactionDetails/Webs
 import { AddressType, RecommendedActionType } from '../../models/simulation/Transaction';
 import { RiskFactors } from './SimulationSubComponents/transactionDetails/RiskFactors';
 import { SimulationBaseProps } from '../../pages/popup';
+import { BsFlagFill, BsThreeDots } from 'react-icons/bs';
+import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from '@chakra-ui/react';
 
 export function TransactionDetails(props: SimulationBaseProps) {
   const { currentSimulation } = props;
+  const [showReportButton, setShowReportButton] = useState(false);
+
+  function toggleReportButton() {
+    if (showReportButton) {
+      closeReportButton();
+    } else {
+      openReportButton();
+    }
+
+    setTimeout(() => {
+      closeReportButton();
+    }, 3000);
+  }
+
+  function openReportButton() {
+    const reportButton = document.querySelector('#reportButton');
+    reportButton?.classList.add(styles.visible);
+    setShowReportButton(true);
+  }
+
+  function closeReportButton() {
+    const reportButton = document.querySelector('#reportButton');
+    reportButton?.classList.remove(styles.visible);
+    setShowReportButton(false);
+  }
 
   return (
     // margin top accounts for the heading + tabs
@@ -21,9 +48,31 @@ export function TransactionDetails(props: SimulationBaseProps) {
             : styles.transactionDetailsCard
         }
       >
-        <p className={styles['heading-md']} style={{ marginBottom: '10px', textTransform: 'none' }}>
-          Transaction Details
-        </p>
+        <div className={styles.row} style={{ justifyContent: "space-between", marginBottom: '10px' }}>
+          <p className={styles['heading-md']} style={{ textTransform: 'none' }}>
+            Transaction Details
+          </p>
+
+          <BsThreeDots style={{ cursor: 'pointer' }} onClick={toggleReportButton} />
+
+          <div id='reportButton' className={styles.reportButton}>
+            <BsFlagFill style={{ marginRight: '5px' }} />
+            <p>Report Transaction</p>
+          </div>
+        </div>
+
+        {/* <Modal isOpen={true} onClose={() => { }} isCentered>
+          <ModalOverlay backdropFilter="blur(1px)" />
+          <ModalContent>
+            <ModalHeader>Are you sure?</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <div style={{ width: '300px', marginLeft: '65px', position: 'absolute', top: '50%', left: '', height: '100px', background: '#212121', borderRadius: '8px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                testing
+              </div>
+            </ModalBody>
+          </ModalContent>
+        </Modal> */}
 
         <div className="row mb-3">
           <div className="col-6">
@@ -53,13 +102,15 @@ export function TransactionDetails(props: SimulationBaseProps) {
           </div>
         </div>
       </div>
-      {currentSimulation.simulation.riskFactors && (
-        <RiskFactors
-          riskFactors={currentSimulation.simulation.riskFactors}
-          recommendedAction={currentSimulation.simulation.recommendedAction}
-          overviewMessage={currentSimulation.simulation.overviewMessage}
-        />
-      )}
-    </div>
+      {
+        currentSimulation.simulation.riskFactors && (
+          <RiskFactors
+            riskFactors={currentSimulation.simulation.riskFactors}
+            recommendedAction={currentSimulation.simulation.recommendedAction}
+            overviewMessage={currentSimulation.simulation.overviewMessage}
+          />
+        )
+      }
+    </div >
   );
 }
