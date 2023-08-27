749b679d6bee7ec85155bc367d8ba1d5922f4e95
mar <72634565+mindofmar@users.noreply.github.com>
2023-08-27 12:34:16 -0500

    add min-width to verification badge (#89)

--- src/components/simulation/PersonalSign.tsx
+++ src/components/simulation/PersonalSign.tsx
@@ -90,7 +90,7 @@ export const PersonalSign: React.FC<PersonalSignProps> = (props: PersonalSignPro
               <div className={styles['df-ac-g5']}>
                 <div
                   className={`${hideOnLoading} ${shimmer}`}
-                  style={{ display: 'flex', height: '18px', width: '18px' }}
+                  style={{ display: 'flex', height: '18px', width: '18px', minWidth: '18px' }}
                 >
                   <WebsiteVerificationBadge verified={currentSimulation.simulation.scanResult.verified} recommendedAction={currentSimulation.simulation.recommendedAction} tooltipPosition='left' />
                 </div>
