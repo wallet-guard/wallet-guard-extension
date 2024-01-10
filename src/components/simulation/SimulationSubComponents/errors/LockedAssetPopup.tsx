c5eb9fdcb910b2633f7fdc79c30a8c379897c14c
mindofmar <martinpeko777@gmail.com>
2024-01-10 12:06:13 -0600

    working on asset changes being locked

--- src/components/simulation/SimulationSubComponents/errors/LockedAssetPopup.tsx
+++ src/components/simulation/SimulationSubComponents/errors/LockedAssetPopup.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from '../../simulation.module.css';
+import posthog from 'posthog-js';
 
 export function LockedAssetPopup() {
+  useEffect(() => {
+    posthog.capture('locked asset warning shown');
+  }, []);
+
   return (
     <div className='container'>
       <div style={{ width: '100%', borderRadius: '10px', display: 'flex', flexDirection: 'row', alignItems: 'center', background: '#2B2B2B', padding: '15px' }}>
@@ -9,7 +14,7 @@ export function LockedAssetPopup() {
 
         <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '20px' }}>
           <p style={{ fontSize: '20px', marginBottom: '0px' }} className={styles['font-archivo-semibold']}>Locked asset detected</p>
-          <p className={styles['softLockedWarningSubtitle']} style={{ marginBottom: '0px' }}>If this was intentional, visit your dashboard to unlock the collection.</p>
+          <p className={styles['softLockedWarningSubtitle']} style={{ marginBottom: '0px' }}>If this was intentional, visit your dashboard to unlock the asset.</p>
         </div>
       </div>
     </div>
--- src/components/simulation/SimulationSubComponents/stateChangeSubComponents/TokenInfo.tsx
+++ src/components/simulation/SimulationSubComponents/stateChangeSubComponents/TokenInfo.tsx
@@ -3,7 +3,6 @@ import { StateChange } from '../../../../models/simulation/Transaction';
 import styles from '../../simulation.module.css';
 import { add3Dots } from '../StateChangesComponent';
 import { AssetChangeImage } from './AssetChangeImage';
-import { LockIcon } from '@chakra-ui/icons';
 
 export interface StateChangesComponentProps {
   stateChange: StateChange;
@@ -11,31 +10,27 @@ export interface StateChangesComponentProps {
 
 export const TokenInfo = (props: StateChangesComponentProps) => {
   return (
-    <div className={styles.assetChangeLeftColumn} style={{ color: props.stateChange.locked ? '#646464 !important' : '' }}>
+    <div className={styles.assetChangeLeftColumn}>
       <AssetChangeImage imageURL={props.stateChange.logo || props.stateChange.tokenURI} />
-      {props.stateChange.coinmarketcapLink ? (
-        <a href={props.stateChange.coinmarketcapLink} target="_blank" className={`${styles['links']}`}>
-          <h3
-            style={{ color: 'white', fontSize: '18px', marginBottom: 0 }}
-            className={`${styles['font-archivo-bold']} pl-3`}
-          >
-            {props.stateChange.symbol ? add3Dots(props.stateChange.symbol, 14) : 'Unknown'}
-            {props.stateChange.locked &&
-              <LockIcon />
-            }
-          </h3>
-        </a>
-      ) : (
-        <h3
-          style={{ color: 'white', fontSize: '18px', marginBottom: 0 }}
-          className={`${styles['font-archivo-bold']} pl-3`}
-        >
-          {props.stateChange.symbol ? add3Dots(props.stateChange.symbol, 14) : 'Unknown'}
+      <div className='flex column align-center pl-3'>
+        <div className={styles['row']}>
+          <a href={props.stateChange.coinmarketcapLink || 'https://coinmarketcap.com'} target="_blank" className={`${styles['links']}`}>
+            <h3
+              style={{ color: 'white', fontSize: '18px', marginBottom: 0 }}
+              className={`${styles['font-archivo-bold']}`}
+            >
+              {props.stateChange.symbol ? add3Dots(props.stateChange.symbol, 14) : 'Unknown'}
+            </h3>
+          </a>
           {props.stateChange.locked &&
-            <LockIcon />
+            <img src='/images/popup/assetLock.svg' className='pl-1' />
           }
-        </h3>
-      )}
+        </div>
+        {props.stateChange.locked && (
+          <p style={{ marginBottom: 0, color: '#646464' }} className={`${styles['font-archivo-medium']}`}>Locked</p>
+        )}
+      </div>
+
     </div>
   );
 };
