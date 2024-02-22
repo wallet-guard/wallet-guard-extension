e2d75ceb32e0ce07d08ee2e020e52f84b07492d8
AugmentedMode <jacob@walletguard.app>
2024-02-22 12:04:55 -0600

    fix small bug

--- src/services/phishing/currentSiteService.ts
+++ src/services/phishing/currentSiteService.ts
@@ -1,16 +1,15 @@
 import localStorageHelpers from '../../lib/helpers/chrome/localStorage';
 import { WgKeys } from '../../lib/helpers/chrome/localStorageKeys';
-import { PhishingResponse, PhishingResult } from '../../models/PhishingResponse';
+import { PhishingResponse, RecommendedAction } from '../../models/PhishingResponse';
 
 export async function getCurrentSite(): Promise<PhishingResponse> {
   const currentSiteCheck = await localStorageHelpers.get<PhishingResponse | null>(WgKeys.CurrentSite);
   const defaultCurrentSite: PhishingResponse = {
     domainName: '',
-    phishing: PhishingResult.Unknown,
-    warnings: null,
+    recommendedAction: RecommendedAction.None,
     verified: false,
     riskFactors: null,
-    status: 'COMPLETE'
+    status: 'COMPLETE',
   };
 
   return currentSiteCheck ?? defaultCurrentSite;
