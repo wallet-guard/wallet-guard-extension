7b28810fd07bec3ea0c2d764a05f25c144de6b52
mar <72634565+mindofmar@users.noreply.github.com>
2024-03-14 18:02:34 -0500

    Feature: Rescan policy & other fixes (#147)
    * add rescan policy
    
    * add checks on c2 before blocking

--- src/services/http/domainScan.ts
+++ src/services/http/domainScan.ts
@@ -3,13 +3,34 @@ import { PhishingResponse } from '../../models/PhishingResponse';
 import * as httpClient from '../clients/httpClient';
 
 export async function domainScan(url: string): Promise<PhishingResponse | null> {
-  try {
-    const res = await httpClient.get(`${SERVER_URL_PROD}/scan?url=${url}`);
-    const json: PhishingResponse = (await res?.json()) || null;
-    return json;
-  } catch (e) {
-    console.error('unable to fetch phishing result', e);
+  const MAX_RETRIES = 3;
+  let attempt = 0;
+  let lastJson: PhishingResponse | null = null;
+
+  while (attempt < MAX_RETRIES) {
+    try {
+      const res = await httpClient.get(`${SERVER_URL_PROD}/scan?url=${url}`);
+      const json: PhishingResponse = await res?.json();
+
+      // Update lastJson on each successful fetch, whether or not status is IN_PROGRESS
+      lastJson = json;
+
+      if (json && json.status !== "IN_PROGRESS") {
+        return json; // If scan is reaches completion state, return the result immediately
+      }
+    } catch (e) {
+      console.error('unable to fetch phishing result', e);
+      return lastJson;
+    }
+
+    attempt++;
+    if (attempt < MAX_RETRIES) await sleep(20000);
   }
 
-  return null;
+  return lastJson;
 }
+
+
+// Create a promise for the timer
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
