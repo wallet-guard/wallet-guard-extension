8ace029996b2c6cd1d07f72022193dde6e20d104
mindofmar <martinpeko777@gmail.com>
2024-07-18 12:26:48 -0500

    add press release link

--- src/components/simulation/SimulationSubComponents/AcquisitionNotice.tsx
+++ src/components/simulation/SimulationSubComponents/AcquisitionNotice.tsx
@@ -15,6 +15,14 @@ export function AcquisitionNoticeModal({ closeCb }: AcquiredNoticeProps) {
             Our <a className={styles.links} href='https://consensys.io/terms-of-use' target='_blank'>Terms of Service</a> and <a className={styles.links} href='https://consensys.io/privacy-notice' target='_blank'>Privacy Policy</a> have recently changed, please read and acknowledge <a className={styles.links} href='https://www.walletguard.app/blog/weve-been-acquired-what-this-means-for-your-data' target='_blank'>the full privacy notice.</a>
           </p>
 
+          <p className={styles['softLockedWarningSubtitle']} style={{ marginBottom: '0px', marginTop: '10px', fontSize: '14px' }}>
+            <a className={styles.links} style={{ marginBottom: '0px', fontSize: '14px' }} target='_blank' href='https://consensys.io/blog/consensys-acquires-wallet-guard-to-enhance-metamask-security'>
+              Read the Full Press Release
+            </a>
+            {' '}
+            to learn more about our recent acquisition.
+          </p>
+
           <button className={styles.buttonOutline} style={{ marginTop: '10px', textAlign: 'left', width: 'fit-content', paddingTop: '5px', paddingBottom: '5px', paddingLeft: '10px', paddingRight: '10px' }} onClick={closeCb}>Acknowledge</button>
         </div>
       </div>
